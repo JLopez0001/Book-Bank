4b1b152f1e4a001ffde9de05775b8ee357ee0c77
John Lopez <john.lopez3695@gmail.com>
2024-02-07 13:58:35 -0500

    Added grid layout to authors search results and styled book container

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const titleInput = document.querySelector('.title-input')
 const authorInput = document.querySelector('.author-input')
 const authorButton = document.querySelector('.author-btn')
 
+const searchResultsHeading = document.querySelector('.search-results-heading')
+const bookResult = document.querySelector('#books-container')
+
 let url = "https://openlibrary.org"
 let coverUrl = "https://covers.openlibrary.org"
 
@@ -18,8 +21,12 @@ titleButton.addEventListener('click', (e) => {
   searchParam = 'title'
 
   e.preventDefault()
+
   titleInput.style.display = "block"
   authorInput.style.display = "none"
+  searchResultsHeading.style.display = "none"
+  bookResult.style.display = "none"
+
   document.querySelector('.title-input input').focus()
 
 
@@ -32,8 +39,12 @@ authorButton.addEventListener('click', (e) => {
   searchParam = 'author'
 
   e.preventDefault()
+
   authorInput.style.display = "block"
   titleInput.style.display = "none"
+  searchResultsHeading.style.display = "none"
+  bookResult.style.display = "none"
+
   document.querySelector('.author-input input').focus()
 
   
@@ -47,8 +58,10 @@ authorButton.addEventListener('click', (e) => {
 function handleBookSearch (e) {
   e.preventDefault()
   let value = e.target[searchParam].value
+  console.log(value)
   let inputValue = value.toLowerCase().split(' ').join('+')
-  
+  console.log(inputValue)
+
   if(searchParam === 'title'){
     fetch(`${url}${inputValue}`)
     .then(res => res.json())
@@ -62,14 +75,19 @@ function handleBookSearch (e) {
       let editions = res.docs[0].edition_count
       let publish = res.docs[0].first_publish_year
 
+      bookResult.innerHTML = ""
       createBookCard(title, cover, author, editions, publish);
     })
+
     
   } else {
-    fetch(`${url}${inputValue}`)
+
+    fetch(`${url}${inputValue}`)    
       .then(res => res.json())
       .then(res => {
         let hash = {}
+        bookResult.innerHTML = ""
+
         for(let i = 0; i <= 10; i++){
           let title = res.docs[i].title
           let cover  = res.docs[i].cover_i
@@ -80,27 +98,37 @@ function handleBookSearch (e) {
             continue
           }else {
             hash[title] = true
+
             createBookCard(title, cover, author, editions, publish);
+
         }
+
       }
+
     })
   }
 }
 
 function createBookCard(title, cover, author, editions, publish) {
   const template = document.getElementById('book-card-template').content.cloneNode(true);
+
   if(!cover){
-    template.querySelector('.cover-img').src = 'https://www.pngarts.com/files/8/Blank-Book-Cover-PNG-Photo.png'
-    template.querySelector('.cover-img').style = "width: 230px; height: auto;";
+    template.querySelector('.cover-img').src = 'https://tse3.mm.bing.net/th?id=OIP.P-nIodv7WzkQ4wYYPsXWaQAAAA&pid=Api&P=0&h=220'
+    template.querySelector('.cover-img').style = "width: 230px; height: 300px;";
   } else {
     template.querySelector('.cover-img').src = `${coverUrl}/b/id/${cover}-M.jpg`
   }
+    searchResultsHeading.style.display = "block"
+    
     template.querySelector('.title').textContent = title;
     template.querySelector('.author').textContent = `Author: ${author}`;
     template.querySelector('.editions').textContent = `Editions: ${editions}`;
     template.querySelector('.publish').textContent = `First Published: ${publish}`;
 
-    document.getElementById('books-container').appendChild(template)
+   
+    bookResult.appendChild(template)
+    bookResult.style.display = "inline-flex"
+
 }
 
 form.addEventListener('submit', handleBookSearch)
